4b8c9bc58ad6f9ed2829437559c44f0f14ab8190
sharif.syful@gmail.com <sharif.syful@gmail.com>
2022-11-08 21:32:04 +0600

    code updated

--- crawl/main.py
+++ crawl/main.py
@@ -1,1 +1,13 @@
 import requests
+import re
+
+url = "http://books.toscrape.com/index.html"
+response = requests.get(url)
+print(response.ok)
+
+text = response.text
+print(len(text))
+result = re.findall(r'<div class="side_categories">(.*?)</div>', text, re.M | re.DOTALL)
+print(len(result))
+print(type(result))
+print(result)
